c14f7f181e6b58cac1dc452bf98afc2f272f59cc
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-07-24 16:05:21 +0200

    16/24 done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { GhostsListComponent } from './ghosts-list/ghosts-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { GhostDetailsComponent } from './ghost-details/ghost-details.component'; // Add this import
+import { GhostDetailsComponent } from './ghost-details/ghost-details.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [AppComponent, GhostsListComponent, GhostDetailsComponent], // Add GhostDetailsComponent to declarations
+  declarations: [AppComponent, GhostsListComponent, GhostDetailsComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
