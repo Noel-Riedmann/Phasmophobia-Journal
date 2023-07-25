9e7b8149caae86033aee5fc4a2eb52eb6b6228fb
Noel-Riedmann <138024930+Noel-Riedmann@users.noreply.github.com>
2023-07-25 15:24:10 +0200

    Added Evidence Filter, Name Filter, Speed Filter and Sanity Threshold Filter.

--- src/app/ghosts-list/ghosts-list.component.ts
+++ src/app/ghosts-list/ghosts-list.component.ts
@@ -12,14 +12,90 @@ export class GhostsListComponent implements OnInit {
   displayedColumns: string[] = ['Name', 'Evidence', 'Speed', 'SanityThreshold', 'Details'];
   dataSource: MatTableDataSource<Ghost>;
   allGhosts: Ghost[] = [];
-  selectedEvidenceIds: number[] = [];
+  uniqueEvidenceNames: string[] = [];
+  selectedEvidenceNames: string[] = [];
+  uniqueSpeedNames: string[] = [];
+  uniqueSanityThresholdNames: string[] = [];
+  selectedSpeedNames: string[] = [];
+  selectedSanityThresholdNames: string[] = [];
 
   constructor(private ghostService: GhostServiceService) {
     this.dataSource = new MatTableDataSource<Ghost>([]);
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.loadGhosts();
+    this.ghostService.getGhosts().subscribe(ghosts => {
+      let evidenceNames: string[] = [];
+      let speedNames: string[] = [];
+      let sanityThresholdNames: string[] = [];
+      ghosts.forEach(ghost => {
+        ghost.evidence.$values.forEach(evidence => {
+          evidenceNames.push(evidence.name);
+        });
+        ghost.speed.$values.forEach(speed => {
+          speedNames.push(speed.name);
+        });
+        ghost.sanityThreshold.$values.forEach(sanityThreshold => {
+          sanityThresholdNames.push(sanityThreshold.name);
+        });
+      });
+      this.uniqueEvidenceNames = [...new Set(evidenceNames)];
+      this.uniqueSpeedNames = [...new Set(speedNames)].sort((a, b) => {
+        const order = ['Slow', 'Normal', 'Fast'];
+        return order.indexOf(a) - order.indexOf(b);
+      });
+      this.uniqueSanityThresholdNames = [...new Set(sanityThresholdNames)].sort((a, b) => {
+        return parseInt(a) - parseInt(b);
+      });
+    });
+  }
+
+  onEvidenceCheckboxChange(event: MatCheckboxChange) {
+    const evidenceName = event.source.value;
+    if (event.checked) {
+      this.selectedEvidenceNames.push(evidenceName);
+    } else {
+      this.selectedEvidenceNames = this.selectedEvidenceNames.filter(name => name !== evidenceName);
+    }
+    this.updateDisplayedData();
+  }
+
+  onSpeedCheckboxChange(event: MatCheckboxChange) {
+    const speedName = event.source.value;
+    if (event.checked) {
+      this.selectedSpeedNames.push(speedName);
+    } else {
+      this.selectedSpeedNames = this.selectedSpeedNames.filter(name => name !== speedName);
+    }
+    this.updateDisplayedData();
+  }
+
+    onSanityThresholdCheckboxChange(event: MatCheckboxChange) {
+    const sanityThresholdName = event.source.value;
+    if (event.checked) {
+      this.selectedSanityThresholdNames.push(sanityThresholdName);
+    } else {
+      this.selectedSanityThresholdNames = this.selectedSanityThresholdNames.filter(name => name !== sanityThresholdName);
+    }
+    this.updateDisplayedData();
+  }
+
+  updateDisplayedData() {
+    let filteredGhosts = this.allGhosts.filter(ghost => {
+      return (
+        this.selectedEvidenceNames.every(evidenceName => {
+          return ghost.evidence.$values.some(evidence => evidence.name === evidenceName);
+        }) &&
+        this.selectedSpeedNames.every(speedName => {
+          return ghost.speed.$values.some(speed => speed.name === speedName);
+        }) &&
+        this.selectedSanityThresholdNames.every(sanityThresholdName => {
+          return ghost.sanityThreshold.$values.some(sanityThreshold => sanityThreshold.name === sanityThresholdName);
+        })
+      );
+    });
+    this.dataSource.data = filteredGhosts;
   }
 
   loadGhosts() {
@@ -35,59 +111,24 @@ export class GhostsListComponent implements OnInit {
     );
   }
 
-  onEvidenceSelectionChange(event: MatCheckboxChange, evidenceId: number) {
-    console.log('onEvidenceSelectionChange called with:', event, evidenceId);
-    const isChecked = event.checked;
-    if (isChecked) {
-      this.selectedEvidenceIds.push(evidenceId);
-    } else {
-      const index = this.selectedEvidenceIds.indexOf(evidenceId);
-      if (index !== -1) {
-        this.selectedEvidenceIds.splice(index, 1);
-      }
-    }
-    this.applyFilter(new Event(''));
-  }
-
   applyFilter(event: Event) {
     console.log('applyFilter called with:', event);
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.data = this.allGhosts.filter((ghost: Ghost) =>
-      ghost.name.toLowerCase().includes(filterValue.trim().toLowerCase())
-    );
-
-    if (this.selectedEvidenceIds.length > 0) {
-      this.dataSource.data = this.dataSource.data.filter((ghost: Ghost) =>
-        ghost.evidence?.$values?.some((evidence: EvidenceItem) => this.selectedEvidenceIds.includes(evidence.evidenceId))
+    let filteredGhosts = this.allGhosts.filter(ghost => {
+      return (
+        ghost.name.toLowerCase().includes(filterValue.trim().toLowerCase()) &&
+        this.selectedEvidenceNames.every(evidenceName => {
+          return ghost.evidence.$values.some(evidence => evidence.name === evidenceName);
+        }) &&
+        this.selectedSpeedNames.every(speedName => {
+          return ghost.speed.$values.some(speed => speed.name === speedName);
+        }) &&
+        this.selectedSanityThresholdNames.every(sanityThresholdName => {
+          return ghost.sanityThreshold.$values.some(sanityThreshold => sanityThreshold.name === sanityThresholdName);
+        })
       );
-    }
-  }
-
-  getUniqueEvidenceItems(): EvidenceItem[] {
-    const evidenceMap: Map<string, EvidenceItem> = new Map();
-    this.allGhosts.forEach((ghost) => {
-      if (ghost.evidence?.$values) {
-        ghost.evidence.$values.forEach((evidence: EvidenceItem) => {
-          const key = evidence.name.toLowerCase();
-          if (evidenceMap.has(key)) {
-            evidenceMap.get(key)!.name = this.combineUniqueEvidenceNames(
-              evidenceMap.get(key)!.name,
-              evidence.name
-            );
-          } else {
-            evidenceMap.set(key, { ...evidence });
-          }
-        });
-      }
     });
-    return Array.from(evidenceMap.values());
-  }
-
-  combineUniqueEvidenceNames(existingNames: string, newName: string): string {
-    const existingNamesArray = existingNames.split(', ');
-    if (!existingNamesArray.includes(newName)) {
-      existingNamesArray.push(newName);
-    }
-    return existingNamesArray.join(', ');
+    this.dataSource.data = filteredGhosts;
   }
 }
+
